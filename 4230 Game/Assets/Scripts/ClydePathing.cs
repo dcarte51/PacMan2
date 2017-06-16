138123249b0f358ecfae82775efd14400207f4ac
dcarte51 <dcarte51@uncc.edu>
2017-06-16 12:13:33 -0400

    I hope this works

---
+++ 4230 Game/Assets/Scripts/ClydePathing.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+
+public class ClydePathing : MonoBehaviour
+{
+
+    private Vector3 location = new Vector3(0f, 0f, 0f);
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position == location)
+        {
+            float xPos = Random.Range(-24.75f, 31f);
+            float zPos = Random.Range(-28f, 39f);
+
+            Vector3 location = new Vector3(xPos, 0f, zPos);
+        }
+
+
+        transform.GetComponent<NavMeshAgent>().destination = location;
+
+    }
+
+}
---
+++ 4230 Game/Assets/Scripts/GhostTeleport.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostTeleport : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private Vector3 teleport1 = new Vector3(-28f, .5f, -3f);
+    private Vector3 teleport2 = new Vector3(28f, .5f, -3f);
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Teleport1"))
+        {
+            transform.position = teleport2;
+        }
+        if (other.gameObject.CompareTag("Teleport2"))
+        {
+            transform.position = teleport1;
+        }
+    }
+}
