67298759557e3b67be3b0689752b0dd35bace237
Michael Bonessi <onaiwolf97@gmail.com>
2017-06-16 14:42:22 -0400

    Menus/Particles

---
+++ 4230 Game/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    [SerializeField]
+    private Canvas MainMenu;
+
+    [SerializeField]
+    private Canvas ControlMenu;
+
+	void Awake()
+    {
+        ControlMenu.enabled = false;
+        MainMenu.enabled = true;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Controls()
+    {
+        MainMenu.enabled = false;
+        ControlMenu.enabled = true;
+    }
+
+    public void Return()
+    {
+        ControlMenu.enabled = false;
+        MainMenu.enabled = true;
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ 4230 Game/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour {
+
+    [SerializeField] private Canvas pauseMenu;
+
+    void Awake()
+    {
+        pauseMenu.enabled = false;
+        Cursor.visible = false;
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.enabled = true;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    public void GameReturn()
+    {
+        pauseMenu.enabled = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1;
+        Cursor.visible = false;
+    }
+
+    public void MenuReturn()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
