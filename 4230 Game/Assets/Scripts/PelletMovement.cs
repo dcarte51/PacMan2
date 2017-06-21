22892bbecd63ce992b2aa084e321644c81aed4f2
dfurr20 <dfurr20@uncc.edu>
2017-06-21 08:27:09 -0400

    Fixed Pellet Script and Minimap

---
+++ 4230 Game/Assets/Scripts/PelletMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PelletMovement : MonoBehaviour {
+
+	public float amplitudeDiv;
+	public float timeDiv;
+	private Vector3 startPosition;
+
+	void Start()
+	{
+		startPosition = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = startPosition + new Vector3 (0.0f, (Mathf.Sin ((Time.time)/timeDiv))/amplitudeDiv, 0.0f);
+
+	}
+}
